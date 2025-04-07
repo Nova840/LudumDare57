304d01634383fcfacd6ff5fdf69456599a3d5ce6
Nova840 <adamzmudzinski2@gmail.com>
2025-04-07 17:26:09 -0400

    Improve placement area

--- scripts/placement_area.gd
+++ scripts/placement_area.gd
@@ -3,12 +3,14 @@ class_name PlacementArea
 
 
 var batteries_inside: int = 0
+var game_won: bool = false
 
 
 func _on_body_entered(body: Node2D) -> void:
 	if body.get_meta("battery", false):
 		batteries_inside += 1
-		if batteries_inside >= (get_tree().current_scene as Game).batteries_to_win:
+		if not game_won and batteries_inside >= (get_tree().current_scene as Game).batteries_to_win:
+			game_won = true
 			print("foop")
 
 
