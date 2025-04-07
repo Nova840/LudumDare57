f562f2425a1be20d24b70760c6f5013d2e651b42
Nova840 <adamzmudzinski2@gmail.com>
2025-04-07 17:23:13 -0400

    Add batteries and number of batteries to win

--- scripts/game.gd
+++ scripts/game.gd
@@ -1,6 +1,10 @@
 extends Node
 class_name Game
 
+
+@export var batteries_to_win: int = 1
+
+
 func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("Escape"):
 		get_tree().change_scene_to_file("res://scenes/start.tscn")
--- scripts/placement_area.gd
+++ scripts/placement_area.gd
@@ -2,6 +2,16 @@ extends Area2D
 class_name PlacementArea
 
 
+var batteries_inside: int = 0
+
+
 func _on_body_entered(body: Node2D) -> void:
-	if body.get_meta("red_kitty", false):
-		print("foop")
+	if body.get_meta("battery", false):
+		batteries_inside += 1
+		if batteries_inside >= (get_tree().current_scene as Game).batteries_to_win:
+			print("foop")
+
+
+func _on_body_exited(body: Node2D) -> void:
+	if body.get_meta("battery", false):
+		batteries_inside -= 1
