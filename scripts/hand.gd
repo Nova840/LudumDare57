beb74c3dd6d24060aa483e41a73edcdf746419d0
Nova840 <adamzmudzinski2@gmail.com>
2025-04-07 14:24:26 -0400

    Add stretching arm

--- scripts/hand.gd
+++ scripts/hand.gd
@@ -9,23 +9,34 @@ class_name Hand
 @export var rotate_speed: float
 @export var rotate_linear_speed_threshold: float
 @export_flags_2d_physics var pick_up_mask: int
+@export var create_arm_point_distance: float
+
+@onready var arm_point: Node2D = $ArmPoint
+@onready var arm_line: Line2D = $ArmPoint/ArmLine
 
 var holding: RigidBody2D = null
 var holding_local_offset: Vector2
 var bodies_able_to_pick_up: Array[RigidBody2D] = []
 var time_last_hit: float = -INF
+var position_last_arm_point_added: Vector2
+
+
+func _ready() -> void:
+	_add_arm_point()
 
 
 func _process(delta: float) -> void:
-	if is_stunned(): return
-
-	if Input.is_action_just_pressed("Click"):
-		if bodies_able_to_pick_up.size() > 0:
-			var rigidbody: RigidBody2D = bodies_able_to_pick_up[0]
-			holding = rigidbody
-			holding_local_offset = to_local(holding.global_position)
-	elif Input.is_action_just_released("Click"):
-		holding = null
+	if not is_stunned():
+		if Input.is_action_just_pressed("Click"):
+			if bodies_able_to_pick_up.size() > 0:
+				var rigidbody: RigidBody2D = bodies_able_to_pick_up[0]
+				holding = rigidbody
+				holding_local_offset = to_local(holding.global_position)
+		elif Input.is_action_just_released("Click"):
+			holding = null
+	
+	if global_position.distance_to(position_last_arm_point_added) >= create_arm_point_distance:
+		_add_arm_point()
 
 
 func _physics_process(delta: float) -> void:
@@ -60,5 +71,11 @@ func hit(position_hit_from: Vector2) -> void:
 	apply_impulse(hit_impulse * (position_hit_from - global_position).normalized())
 	holding = null
 
+
 func is_stunned() -> bool:
 	return time_last_hit + hit_cooldown * 1000 > Time.get_ticks_msec()
+
+
+func _add_arm_point() -> void:
+	arm_line.add_point(arm_point.global_position)
+	position_last_arm_point_added = arm_point.global_position
