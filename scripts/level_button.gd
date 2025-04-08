c1150f2909b6fe271625bae32335d14b443ab00e
Nova840 <adamzmudzinski2@gmail.com>
2025-04-07 21:49:32 -0400

    Do start screen stuff and move music to global

--- scripts/level_button.gd
+++ scripts/level_button.gd
@@ -2,8 +2,13 @@ extends Button
 class_name LevelButton
 
 
+@export var drawer_open_sound: PackedScene
+
 @export_file var level_path: String
 
 
 func _on_pressed() -> void:
+	var sound := drawer_open_sound.instantiate()
+	add_child(sound)
+	await get_tree().create_timer(1).timeout
 	get_tree().change_scene_to_file(level_path)
