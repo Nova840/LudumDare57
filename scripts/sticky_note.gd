e71678e51a0dc267e0c46c24aa09f4158d344720
Nova840 <adamzmudzinski2@gmail.com>
2025-04-08 22:45:24 -0400

    Fix sticky note probability

--- scripts/sticky_note.gd
+++ scripts/sticky_note.gd
@@ -4,6 +4,7 @@ class_name StickyNote
 
 @export var base_colors: Array[Color]
 @export var overlay_textures: Array[Texture2D]
+@export_range(0, 1) var chance_of_overlay: float
 
 @onready var base: Sprite2D = $Base
 @onready var overlay: Sprite2D = $Overlay
@@ -13,4 +14,6 @@ class_name StickyNote
 
 func _ready() -> void:
 	base.self_modulate = base_colors[random.randi_range(0, base_colors.size() - 1)]
-	overlay.texture = overlay_textures[random.randi_range(0, overlay_textures.size() - 1)]
+	var has_image: bool = random.randf() < chance_of_overlay
+	if has_image:
+		overlay.texture = overlay_textures[random.randi_range(0, overlay_textures.size() - 1)]
