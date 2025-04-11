4d479e1044745c57736a7dcb362013fe74b9fc37
Nova840 <adamzmudzinski2@gmail.com>
2025-04-11 18:05:01 -0400

    Canvas items stretch mode and confine cursor

--- scripts/camera.gd
+++ scripts/camera.gd
@@ -10,7 +10,7 @@ class_name Camera
 
 
 func _process(delta: float) -> void:
-	var screen_middle_position := get_canvas_transform().affine_inverse() * (get_tree().root.size as Vector2 / 2)
+	var screen_middle_position := get_screen_center_position()
 	var speed_multiplier := clampf((screen_middle_position.distance_to(follow.global_position) - move_deadzone) / 100, 0, 1)
 	var target_global_position := global_position.lerp(follow.global_position, clampf(follow_speed * speed_multiplier * delta, 0, 1))
 	global_position = global_position.move_toward(target_global_position, follow_max_speed * delta)
