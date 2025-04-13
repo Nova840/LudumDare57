88b5216f997171c9745e9ef3a8b010ea12f70c4d
Nova840 <adamzmudzinski2@gmail.com>
2025-04-12 22:16:21 -0400

    Make camera movement smoother

--- scripts/camera.gd
+++ scripts/camera.gd
@@ -9,7 +9,7 @@ class_name Camera
 @export var move_deadzone: float
 
 
-func _process(delta: float) -> void:
+func _physics_process(delta: float) -> void:
 	var screen_middle_position := get_screen_center_position()
 	var speed_multiplier := clampf((screen_middle_position.distance_to(follow.global_position) - move_deadzone) / 100, 0, 1)
 	var target_global_position := global_position.lerp(follow.global_position, clampf(follow_speed * speed_multiplier * delta, 0, 1))
