4bc9adcdab720acab8e56f2474f606200a12237a
Nova840 <adamzmudzinski2@gmail.com>
2025-04-23 03:26:51 -0400

    Add drawer sneaky side fade out

--- scripts/camera_move_area.gd
+++ scripts/camera_move_area.gd
@@ -2,6 +2,13 @@ extends Area2D
 class_name CameraMoveArea
 
 
+@export var fade_out_sprite: Sprite2D
+
+
 func _on_body_entered(body: Node2D) -> void:
-	if body is Hand:
-		Camera.instance.can_move = true
+	if body is not Hand: return
+	Camera.instance.can_move = true
+	var fade_percent := 1.0
+	while fade_percent > 0:
+		await get_tree().process_frame
+		fade_out_sprite.modulate.a -= get_process_delta_time()
